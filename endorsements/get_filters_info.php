b406029454cafa30a5ba7964bc4661ee9173093b
Robert Lefebvre <robert.r.lefebvre@gmail.com>
2021-07-22 14:08:16 -0400

    added endorsements folder from local

---
+++ endorsements/get_filters_info.php
@@ -0,0 +1,10 @@
+
+
+<div id="openModal" class="modalWindow" width="800px" height="600px">
+        <div class="modalHeader"><h1 align="center">Info About Sub categories And Location Filters</h1></div>
+            <p align="left">There are two expanding dropdown menus that operate together to give the viewer control of the selection of the next page. The left menu determines the next category to return results from. The right one will add additional location filtering to whichever category is selected.</p>
+            <p  align="left">You can go back and forth between the two menus and adjust your selection as often as you wish and the "Go" button in between them will adjust the url accordingly. When you are through making your selection then click the "Go" button and retrieve the results for the category, the location or a combination of both.
+<h1 style="text-align:center;">Close this window when finished</h1>
+         
+</div>     
+            <div class="clear"></div>
