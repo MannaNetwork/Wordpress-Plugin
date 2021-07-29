02e30c8d77fbf42d55c73570e2e0e4c026a242c8
3ND0fD4Y5 <87153476+3ND0fD4Y5@users.noreply.github.com>
2021-07-29 18:39:27 -0400

    Update en_no_registration.php

--- translations/en_no_registration.php
+++ translations/en_no_registration.php
@@ -1,6 +1,5 @@
 <?php
 
-
 echo '<h2>Configuration Status</h2><h3 style="color:red;">We were not able to detect that your website "'.$http_host.'" is registered as an advertiser in the Manna Network (which is required in order for you to configure the plugin). 
 </h3>
 <h3 style="color:red;">If you found and downloaded the plugin from one of our members or found out about the Manna Network from one then, please, register at that member site (visit their site, find their Manna Network display page and click the "Add URL" link)!</h3>
@@ -8,8 +7,5 @@ echo '<h2>Configuration Status</h2><h3 style="color:red;">We were not able to de
 <h3>Registration is free!</h3>
 <h3>AND it will send traffic to your website from the growing network of other websites using the plugin!</h3>
 <h3>After registering, refresh this page.</h3>';
-
-
-//echo '<img src = "/wp-content/plugins/manna-network/translations/screenshot.png">';
 
 ?>
