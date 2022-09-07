e69253434153edf51971d9d79a1e4d13b3cd3933
MannaNetwork <robert.r.lefebvre@gmail.com>
2022-09-07 19:18:38 -0400

    deleted new folder

--- new/endorsements/register.php
+++
@@ -1,3 +0,0 @@
-<?php
-include("views/register.php");
-?>
--- new/js/mn_main_page.js
+++
@@ -1,8 +0,0 @@
-<script>
-
- function select_main_category ( selected_category )
-{
-  document.main_category_form.category_id.value = selected_category ;
-  document.main_category_form.submit() ;
-}
-</script>
